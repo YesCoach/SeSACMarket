a75a16d1575eb7ae96d366f1e650f4f84db73552
YesCoach <sbsii1020@gmail.com>
2023-09-08 15:39:00 +0900

    feat: Goods 도메인 모델 타입 구현

---
+++ SeSACMarket/Domain/Goods.swift
@@ -0,0 +1,31 @@
+//
+//  Goods.swift
+//  SeSACMarket
+//
+//  Created by 박태현 on 2023/09/08.
+//
+
+import Foundation
+
+// MARK: - GoodsSearchResult
+
+struct GoodsSearchResult {
+    let lastBuildDate: String?
+    let total: Int?
+    let start: Int?
+    let display: Int?
+    let items: [Goods]?
+}
+
+// MARK: - Goods
+struct Goods {
+    let productID: String?
+    let title: String
+    let link: String?
+    let image: String?
+    let lowPrice: String?
+    let highPrice: String?
+    let mallName: String?
+    let brand: String?
+    let maker: String?
+}
